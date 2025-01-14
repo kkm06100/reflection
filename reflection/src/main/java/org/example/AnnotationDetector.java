1fbfba9f9a63ff6754e3d6ec9573ba10c1e844a8
하이 <kkm07133@naver.com>
2025-01-15 00:22:34 +0900

    순환 참조 가능 테스트

--- reflection/src/main/java/org/example/AnnotationDetector.java
+++ reflection/src/main/java/org/example/AnnotationDetector.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AnnotationDetector {
+    /**
+     * 지정한 어노테이션이 존재하는지 확인해주는 메소드
+     */
     public static boolean hasAnnotation(AnnotatedElement element, Class<? extends Annotation> targetAnnotation){
         final Set<Class<? extends Annotation>> visited = new HashSet<>();
         for (Annotation annotation : element.getAnnotations()) {
@@ -15,6 +18,10 @@ public class AnnotationDetector {
         }
         return false;
     }
+
+    /**
+     * 재귀적 호출을 통해서 자기 자신이 가지고 있는 코드가 targetAnnotation 인지 확인하는 메소드
+     */
     private static boolean isAnnotationPresent(Class<? extends Annotation> annotationType, Class<? extends Annotation> targetAnnotation, Set<Class<? extends Annotation>> visited){
         visited.add(annotationType);
         if(annotationType.equals(targetAnnotation)){
@@ -29,13 +36,28 @@ public class AnnotationDetector {
         }
         return false;
     }
+
     public static void main(String[] args) {
+        // @CustomComponent -> @Component
         if(hasAnnotation(TestService.class, Component.class)){
-            System.out.println("including @Component");
+            System.out.println(TestService.class.getName() + " including @Component");
+        }else{
+            System.out.println(TestService.class.getName() + " not including @Component");
         }
 
-        if(!hasAnnotation(FieldUsage.class, Component.class)){
-            System.out.println("not including @Component");
+        // @Component X
+        if(hasAnnotation(FieldUsage.class, Component.class)){
+            System.out.println(FieldUsage.class.getName() + " including @Component");
+        }else{
+            System.out.println(FieldUsage.class.getName() + " not including @Component");
+        }
+
+        // @Annotation Cycle
+        // non-stack overflow code
+        if(hasAnnotation(CycleComponent2.class, Component.class)){
+            System.out.println(CycleComponent2.class.getName() + " including @Component");
+        }else{
+            System.out.println(CycleComponent2.class.getName() + " not including @Component");
         }
     }
 }
---
+++ reflection/src/main/java/org/example/CycleComponent.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@CustomComponent
+@Component
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@CycleComponent2
+public @interface CycleComponent {
+}
