fec3a7935df3a60e3c4f2e16dccab2997d7e2abf
하이 <kkm07133@naver.com>
2025-01-14 12:05:50 +0900

    Method 사용법

--- reflection/src/main/java/org/example/ClassLoad.java
+++ reflection/src/main/java/org/example/ClassLoad.java
@@ -4,24 +4,23 @@ package org.example;
  * Class 객체 로딩 방법
  */
 public class ClassLoad {
-    public static void main(String[] args) {
-        // Object.getClass()
-        String str = new String("Class test");
-        Class<?> cls = str.getClass();
-        System.out.println(cls);
-
-        // class 리터럴
-        Class<String> cls1 = String.class;
-        System.out.println(cls1);
-
-        // Class.forName()으로 얻기
-        try{
-            Class<?> cls2 = Class.forName("java.lang.String");
-            System.out.println(cls2);
-        }catch (ClassNotFoundException e){
-
-        }
-
-
-    }
+//    public static void main(String[] args) {
+//        // Object.getClass()
+//        String str = new String("Class test");
+//        Class<?> cls = str.getClass();
+//        System.out.println(cls);
+//
+//        // class
+//        Class<String> cls1 = String.class;
+//        System.out.println(cls1);
+//
+//        try{
+//            Class<?> cls2 = Class.forName("java.lang.String");
+//            System.out.println(cls2);
+//        }catch (ClassNotFoundException e){
+//
+//        }
+//
+//
+//    }
 }
---
+++ reflection/src/main/java/org/example/MethodUsage.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class MethodUsage {
+    public static void main(String[] args) throws Exception{
+        Class<Person> personClass = Person.class;
+
+        Constructor<Person> constructor = personClass.getConstructor(String.class, int.class);
+
+        Person person = constructor.newInstance("kkm",18);
+        person.getField();
+
+        // method
+        Method sum = personClass.getMethod("sum", int.class, int.class);
+        System.out.println(sum.invoke(new Person(), 100, 100));
+
+        // static method
+        Method staticSum = personClass.getMethod("staticSum", int.class, int.class);
+        System.out.println(sum.invoke(null, 100, 100));
+
+        //private method
+        Method privateSum = personClass.getDeclaredMethod("privateSum", int.class, int.class);
+        System.out.println(privateSum.invoke(new Person(), 100, 100));
+
+    }
+}
