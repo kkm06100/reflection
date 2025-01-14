d1e27b972d964e3c60d3edd38a1a0b3d1255f041
하이 <kkm07133@naver.com>
2025-01-14 23:19:11 +0900

    Reflection 사용

---
+++ reflection/src/main/java/org/example/ReflectionUsage.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.reflections.Reflections;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+public class ReflectionUsage {
+    public static void main(String[] args) {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+
+        for(Class<?> clazz : classes){
+            System.out.println("found class: " + clazz.getName());
+
+            Object instance;
+            try{
+                instance = clazz.getDeclaredConstructor().newInstance();
+                System.out.println("Created instance of: " + clazz.getName());
+            }catch (Exception e){
+                System.out.println("Couldn't instantiate: " + clazz.getName());
+                continue;
+            }
+            if(clazz.isAnnotationPresent(At.class)){
+
+            }
+
+        }
+    }
+}
